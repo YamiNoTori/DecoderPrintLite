a7951029ac22ce4ca4ec54bea310792c6ea36fca
Dmitriy <YamiNoTori>
2022-08-09 14:57:34 +0300

    new gitignore

---
+++ DecoderPrintLiteLibs/EncodingController.cs
@@ -0,0 +1,14 @@
+
+
+
+namespace DecoderPrintLiteLibs
+{
+    // this class contains data about algoritms and launch them
+    public class EncodingController
+    {
+
+
+
+        public void Start() {}
+    }
+}
