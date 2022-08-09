0c0bf3fad275174f2dcd6e4b1feca71a76953625
Dmitriy <YamiNoTori>
2022-08-09 14:47:21 +0300

    added encoding menu

--- DecoderPrintLiteView/Program.cs
+++ DecoderPrintLiteView/Program.cs
@@ -9,7 +9,100 @@ namespace DevoderPrintView
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Start
+            Console.WriteLine("\t\nDECODER PRINT LITE\n\n\nPlease press any button to continue...");
+            Console.ReadKey();
+            
+
+            int input;
+            bool inputStatus;
+            while(true)
+            {
+                // Main menu
+                Console.Clear();
+                Console.Write("\n1. Encryption\n2. Decoding\n\nEnter menu point: ");
+                inputStatus = Int32.TryParse(Console.ReadLine(), out input);
+                if (!inputStatus)
+                {
+                    Console.WriteLine($"Incorrect value!\nPress any button to continue...");
+                    Console.ReadKey();
+                    continue;
+                }
+                switch(input)
+                {
+                    case 1:
+                        GoToEncodingPage();
+                        break;
+                    case 2: 
+                        GoToDecodingPage();
+                        break;
+                    default:
+                        Console.WriteLine($"Point {input} missing from menu!\nPress any button to continue...");
+                        Console.ReadKey();
+                        continue;
+                }
+            }
+
+
+
         }
+
+        // encoding page
+        public static void GoToEncodingPage()
+        {
+            int input;
+            bool inputStatus;            
+            while(true)
+            {
+                Console.Clear();
+                Console.Write("\n\tPlease, choose encoding algoritm\n\n"+
+                "\t1. Cesar\n"+
+                "\t2. XOR-encoding\n"+
+                "\t3. Playfair cipher\n\n"+
+                
+                "\tAlgorimt point: ");
+                inputStatus = Int32.TryParse(Console.ReadLine(), out input);
+                if (!inputStatus)
+                {
+                    Console.WriteLine($"Incorrect value!\nPress any button to continue...");
+                    Console.ReadKey();
+                    continue;
+                }
+                switch(input)
+                {
+                    case 1:
+   
+                        break;
+                    case 2: 
+
+                        break;
+                    case 3:
+
+                        break;
+                    default:
+                        Console.WriteLine($"Point {input} missing from menu!\nPress any button to continue...");
+                        Console.ReadKey();
+                        continue;
+                }
+                Console.ReadKey();
+                System.Environment.Exit(-1);
+
+            }
+
+            // Console.ReadKey();
+            // System.Environment.Exit(-1);
+        }
+
+        // decoding page
+        public static void GoToDecodingPage()
+        {
+            Console.Clear();
+            Console.WriteLine("Decoding...");
+            Console.ReadKey();
+            System.Environment.Exit(-1);
+        }
+
+
+
     }
 }
